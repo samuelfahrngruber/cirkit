886a4e55ec4f9aa88c660399746e854b11ee4ca7
Samuel Fahrngruber <samuel.fahrngruber-biernbaum@student.tugraz.at>
2025-12-02 14:44:55 +0100

    feat(em): more concise

--- cirkit/backend/torch/training/em.py
+++ cirkit/backend/torch/training/em.py
@@ -1,97 +1,111 @@
 from abc import ABC
-from typing import Mapping, Generic, TypeVar
+from typing import Generic, TypeVar, override
 
 import torch
-from black.trans import defaultdict
 from torch import Tensor
 
 from cirkit.backend.torch.circuits import TorchCircuit
-from cirkit.backend.torch.layers import TorchSumLayer, TorchGaussianLayer, TorchLayer
+from cirkit.backend.torch.layers import TorchSumLayer, TorchGaussianLayer, TorchLayer, TorchHadamardLayer
 from cirkit.backend.torch.parameters.nodes import TorchTensorParameter
 from cirkit.backend.torch.parameters.parameter import TorchParameter
 
-L = TypeVar("L", bound=TorchLayer)
+AnyTorchSumLayer = TorchSumLayer
+
 
 def update_params_nested(params: TorchParameter, new_values: torch.Tensor):
     for p in params.outputs:
         if type(p) == TorchTensorParameter:
             p.update_params(new_values.squeeze())
 
-class AbstractEMCell(Generic[L], ABC):
+
+L = TypeVar("L", bound=TorchLayer)
+
+
+class AbstractLayerEM(Generic[L], ABC):
+
     def __init__(self, layer: L):
         self.layer = layer
         self.sufficient_statistics: dict[str, Tensor] = {}
 
+    def layer_fn(self, layer, *inputs):
+        return layer(*inputs)
+
+    def expectation(self):
+        raise NotImplementedError()
+
+    def maximization(self):
+        raise NotImplementedError()
+
+
+class NoopLayerEM(AbstractLayerEM[TorchLayer]):
+
+    def __init__(self, layer: TorchLayer):
+        super().__init__(layer)
+
+    @override
+    def expectation(self):
+        pass
+
+    @override
+    def maximization(self):
+        pass
+
+
+class SumLayerEM(AbstractLayerEM[AnyTorchSumLayer]):
+
+    def __init__(self, layer: AnyTorchSumLayer):
+        super().__init__(layer)
+        self.weight = self.layer.weight.outputs[0]._ptensor
+
+    @override
+    def expectation(self):
+        self.sufficient_statistics["n"] = self.weight.grad.sum(dim=2)
+
+    @override
+    def maximization(self):
+        n = self.sufficient_statistics["n"]
+        new_weight = self.weight * n / n.sum()
+
+        update_params_nested(self.layer.weight, new_weight)
+        self.weight.copy_(new_weight)
+
+
+class GaussianLayerEM(AbstractLayerEM[TorchGaussianLayer]):
+
+    def __init__(self, layer: TorchGaussianLayer):
+        super().__init__(layer)
+        self.mean = self.layer.params["mean"]()
+        self.stddev = self.layer.params["stddev"]()
+        self.inputs = None
+        self.outputs = None
+
+    @override
     def layer_fn(self, layer, *inputs):
         self.inputs = torch.stack(inputs)
         self.outputs = layer(*inputs)
         self.outputs.retain_grad()
         return self.outputs
 
+    @override
     def expectation(self):
-        raise NotImplementedError()
+        p_l = self.outputs.grad.permute(2, 1, 0)  # [Inputs, Samples, Outputs] -> [Outputs, Samples, Inputs]
 
-    def maximization(self):
-        raise NotImplementedError()
-
-
-class SumEMCell(AbstractEMCell[TorchSumLayer]):
-    def __init__(self, layer: TorchSumLayer):
-        super().__init__(layer)
-        self.weight = self.layer.weight.outputs[0]._ptensor
-
-    def expectation(self):
-        self.sufficient_statistics["n"] = self.weight.grad.sum(dim=2)
-
-    def maximization(self):
-        n = self.sufficient_statistics["n"]
-        new_weight = self.weight * n / n.sum()
-        print(f"{new_weight=}")
-
-        update_params_nested(self.layer.weight, new_weight)
-        self.weight.copy_(new_weight)
-
-
-class GaussianEMCell(AbstractEMCell[TorchGaussianLayer]):
-    def __init__(self, layer: TorchGaussianLayer):
-        super().__init__(layer)
-        self.mean = self.layer.params["mean"]()
-        self.stddev = self.layer.params["stddev"]()
-
-    def expectation(self):
-        p_l = self.outputs.grad # [Inputs, Samples, Outputs]
-        p_l = p_l.permute(2, 1, 0)  # [Outputs, Samples, Inputs]
-
-        # old impl:
-        # x = data_full[:,leaf.scope_idx[-1]] # [Samples, Features]
-        data_full = self.inputs
-        x = self.inputs # data_full[..., self.layer.scope_idx].permute(1, 0, 2)  # [???] taken from cirkit/backend/torch/circuits.py:66
+        x = self.inputs  # [Samples, Features]
         x_2 = x ** 2  # [Samples, Features]
 
-        sum_x_suff_stats_x = torch.sum(p_l * x, dim=2)  # [Outputs, Inputs]
-        sum_x_suff_stats_x_2 = torch.sum(p_l * x_2, dim=2)  # [Outputs, Inputs]
+        self.sufficient_statistics["x"] = torch.sum(p_l * x, dim=2)  # [Outputs, Inputs]
+        self.sufficient_statistics["x^2"] = torch.sum(p_l * x_2, dim=2)  # [Outputs, Inputs]
+        self.sufficient_statistics["p_l"] = torch.sum(p_l, dim=1)  # [Outputs, Inputs]
 
-        sum_x_p_l = torch.sum(p_l, dim=1)  # [Outputs, Inputs]
-
-        self.sufficient_statistics["x"] = sum_x_suff_stats_x
-        self.sufficient_statistics["x^2"] = sum_x_suff_stats_x_2
-        self.sufficient_statistics["p_l"] = sum_x_p_l
-
+    @override
     def maximization(self):
-        sum_x_suff_stats_x = self.sufficient_statistics["x"] # [Outputs, Inputs]
-        sum_x_suff_stats_x_2 = self.sufficient_statistics["x^2"]  # [Outputs, Inputs]
-        sum_x_p_l = self.sufficient_statistics["p_l"]  # [Outputs, Inputs]
-
-        x = sum_x_suff_stats_x / sum_x_p_l  # [Outputs, Inputs]
-        x_2 = sum_x_suff_stats_x_2 / sum_x_p_l  # [Outputs, Inputs]
+        p_l = self.sufficient_statistics["p_l"]  # [Outputs, Inputs]
+        x = self.sufficient_statistics["x"] / p_l  # [Outputs, Inputs]
+        x_2 = self.sufficient_statistics["x^2"] / p_l  # [Outputs, Inputs]
 
         mean = x  # [Outputs, Inputs]
         var = x_2 - x ** 2  # [Outputs, Inputs]
-        var = var.clamp(min=1e-3)  # [Outputs, Inputs]
-
         stddev = torch.sqrt(var)  # [Outputs, Inputs]
-
-        print(f"{mean=} {var=}")
 
         update_params_nested(self.layer.params["mean"], mean)
         update_params_nested(self.layer.params["stddev"], stddev)
@@ -100,22 +114,24 @@ class GaussianEMCell(AbstractEMCell[TorchGaussianLayer]):
         self.stddev.copy_(stddev.squeeze())
 
 
-def create_em_cell(layer: TorchLayer) -> AbstractEMCell[TorchLayer] | None:
+def create_layer_em(layer: TorchLayer) -> AbstractLayerEM[TorchLayer]:
+    if layer.num_parameters == 0:
+        return NoopLayerEM(layer)
     match layer:
         case TorchSumLayer():
-            return SumEMCell(layer)
+            return SumLayerEM(layer)
         case TorchGaussianLayer():
-            return GaussianEMCell(layer)
-
-
+            return GaussianLayerEM(layer)
+    raise ValueError(f"EM is not supported for layer: {layer}")
 
 
 class FullBatchEM:
+
     def __init__(self, circuit: TorchCircuit):
         self.circuit = circuit
-        self.em_cells: dict[TorchLayer, AbstractEMCell[TorchLayer]] = {}
+        self.layer_ems: dict[TorchLayer, AbstractLayerEM[TorchLayer]] = {}
         for layer in circuit.layers:
-            self.em_cells[layer] = create_em_cell(layer)
+            self.layer_ems[layer] = create_layer_em(layer)
         self.log_likelihoods_per_sample = None
 
     def zero_grad(self):
@@ -126,22 +142,19 @@ class FullBatchEM:
         return self.log_likelihoods_per_sample
 
     def layer_fn(self, layer, *inputs):
-        em_cell = self.em_cells.get(layer)
-        if em_cell:
-            return em_cell.layer_fn(layer, *inputs)
-        return layer(*inputs)
+        return self.layer_ems[layer].layer_fn(layer, *inputs)
 
-    def backward(self):
+    def backward_latent_posterior(self):
         self.log_likelihoods_per_sample.sum().backward()
 
     def expectation(self):
         with torch.no_grad():
-            for layer, cell in self.em_cells.items():
-                if cell:
-                    cell.expectation()
+            for layer, em in self.layer_ems.items():
+                if em:
+                    em.expectation()
 
     def maximization(self):
         with torch.no_grad():
-            for layer, cell in self.em_cells.items():
-                if cell:
-                    cell.maximization()
+            for layer, em in self.layer_ems.items():
+                if em:
+                    em.maximization()
